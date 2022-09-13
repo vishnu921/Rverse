2f4c58d66dc4137a85d0a9a8837a5726479db1b6
vishnu0921 <ba11@iitbbs.ac.in>
2022-09-13 20:51:22 +0530

    frontend deploy

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({baseURL: 'http://localhost:4000/api/'});
+const API = axios.create({baseURL: 'https://rverse.herokuapp.com/api/'});
 
 API.interceptors.request.use((req)=>{
     const user = JSON.parse(localStorage.getItem('profile'));
